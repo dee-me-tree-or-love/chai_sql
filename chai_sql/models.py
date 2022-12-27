6e606d67c55c60ff5b5606599f4b5cf87363225a
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-12-27 12:51:02 +0100

    feat: refactor use of SqlASt

--- chai_sql/models.py
+++ chai_sql/models.py
@@ -1,6 +1,19 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Generic, Iterator, Protocol, Sequence, Tuple, TypeAlias, TypeVar
+from itertools import chain
+from typing import (
+    Any,
+    Generic,
+    Iterator,
+    List,
+    Protocol,
+    Sequence,
+    Tuple,
+    TypeAlias,
+    TypeVar,
+)
 
 T = TypeVar("T")
 
@@ -49,6 +62,39 @@ class RawSqlAst(Generic[T]):
     def tokens(self) -> Iterator[RawSqlToken]:
         # TODO: make this an abstract function
         raise NotImplementedError()
+
+
+class KnownSqlCommand(Enum):
+    NOT_SUPPORTED = 0
+    SELECT = 1
+
+
+@dataclass
+class SqlAstNode:
+    children: Iterator[SqlAstNode]
+    nr_children: int
+    value: str
+    # TODO: restrict the token family
+    family: str
+
+
+@dataclass
+class SqlAst(Generic[T]):
+    source: T
+    command: KnownSqlCommand
+
+    def roots(self) -> Iterator[SqlAstNode]:
+        # TODO: make this an abstract function
+        raise NotImplementedError()
+
+    def dfs_nodes(self) -> Iterator[SqlAstNode]:
+        return chain.from_iterable(self.__dfs_iter(node) for node in self.roots())
+
+    def __dfs_iter(self, node: SqlAstNode) -> Iterator[SqlAstNode]:
+        yield node
+        for child in node.children:
+            for c_node in self.__dfs_iter(child):
+                yield c_node
 
 
 class ChaiSqlAst:
--- chai_sql/sql/query_parser.py
+++ chai_sql/sql/query_parser.py
@@ -4,7 +4,7 @@ from typing import Generic, Iterator, Sequence, TypeVar
 #   https://colab.research.google.com/drive/100hW8Qw5iSNafhi54Ucriw1SA0bdBgjX?usp=sharing
 import sqlparse
 
-from chai_sql.models import KnownSqlOptions, RawSqlAst, RawSqlToken
+from chai_sql.models import KnownSqlCommand, RawSqlAst, RawSqlToken, SqlAst, SqlAstNode
 
 T = TypeVar("T")
 
@@ -37,89 +37,96 @@ def _get_sqlparse_asts(t: str) -> Sequence[sqlparse.sql.Statement]:
     return sqlparse.parse(t)
 
 
-def _sqlparse_statement_2_known_option(
+def _sqlparse_statement_2_known_command(
     s: sqlparse.sql.Statement,
-) -> KnownSqlOptions:
+) -> KnownSqlCommand:
     """
     Determines whether the passed SQL statement is a known option.
 
     Examples:
         >>> simple_sql = "select * from foo;"
         >>> statement_1 = _get_sqlparse_asts(simple_sql)[0]
-        >>> _sqlparse_statement_2_known_option(statement_1)
-        <KnownSqlOptions.SELECT: 1>
+        >>> _sqlparse_statement_2_known_command(statement_1)
+        <KnownSqlCommand.SELECT: 1>
 
         >>> complex_sql = "insert into foo values 'baz';"
         >>> statement_2 = _get_sqlparse_asts(complex_sql)[0]
-        >>> _sqlparse_statement_2_known_option(statement_2)
-        <KnownSqlOptions.NOT_SUPPORTED: 0>
+        >>> _sqlparse_statement_2_known_command(statement_2)
+        <KnownSqlCommand.NOT_SUPPORTED: 0>
     """
     try:
-        return KnownSqlOptions[s.get_type().upper()]
+        return KnownSqlCommand[s.get_type().upper()]
     except KeyError:
-        # TODO: add logging
-        return KnownSqlOptions.NOT_SUPPORTED
+        # TODO(tech-debt): add logging
+        return KnownSqlCommand.NOT_SUPPORTED
 
 
-class SqlParseRawSqlAst(RawSqlAst[sqlparse.sql.Statement], Generic[T]):
-    def tokens(self):
+class SqlParseSqlAst(SqlAst[sqlparse.sql.Statement], Generic[T]):
+    def roots(self):
         """
         Examples:
             >>> simple_sql = "select * from foo;"
-            >>> statement_1 = _get_sqlparse_asts(simple_sql)[0]
-            >>> sql_tree = SqlParseRawSqlAst(KnownSqlOptions.SELECT, statement_1)
-            >>> tokens = sql_tree.tokens()
-            >>> next(tokens)
-            RawSqlToken(position=(0, 0), option='...Keyword.DML', value='select')
-            >>> next(tokens)
-            RawSqlToken(position=(1, 0), option='...Whitespace', value=' ')
-            >>> next(tokens)
-            RawSqlToken(position=(2, 0), option='...Wildcard', value='*')
-            >>> next(tokens)
-            RawSqlToken(position=(3, 0), option='...Whitespace', value=' ')
-            >>> next(tokens)
-            RawSqlToken(position=(4, 0), option='...Keyword', value='from')
-            >>> next(tokens)
-            RawSqlToken(position=(5, 0), option='...Whitespace', value=' ')
-            >>> next(tokens)
-            RawSqlToken(position=(6, 0), option='...Name', value='foo')
-            >>> next(tokens)
-            RawSqlToken(position=(7, 0), option='...Punctuation', value=';')
-            >>> try: next(tokens)
+            >>> statement = _get_sqlparse_asts(simple_sql)[0]
+            >>> sql_tree = SqlParseSqlAst(statement, KnownSqlCommand.SELECT)
+            >>> children = sql_tree.roots()
+            >>> next(children)
+            SqlAstNode(children=<...>, nr_children=0, value='select', family='Token.Keyword.DML')
+            >>> next(children)
+            SqlAstNode(children=<...>, nr_children=0, value=' ', family='Token.Text.Whitespace')
+            >>> next(children)
+            SqlAstNode(children=<...>, nr_children=0, value='*', family='Token.Wildcard')
+            >>> next(children)
+            SqlAstNode(children=<...>, nr_children=0, value=' ', family='Token.Text.Whitespace')
+            >>> next(children)
+            SqlAstNode(children=<...>, nr_children=0, value='from', family='Token.Keyword')
+            >>> next(children)
+            SqlAstNode(children=<...>, nr_children=0, value=' ', family='Token.Text.Whitespace')
+            >>> n = next(children)
+            >>> n
+            SqlAstNode(children=<...>, nr_children=1, value='foo', family='None')
+            >>> next(n.children)
+            SqlAstNode(children=<...>, nr_children=0, value='foo', family='Token.Name')
+            >>> next(children)
+            SqlAstNode(children=<...>, nr_children=0, value=';', family='Token.Punctuation')
+            >>> try: next(children)
             ... except StopIteration: print('Iterator exhausted')
             Iterator exhausted
         """
-        return (
-            RawSqlToken((index_group, index_elmnt), str(token.ttype), token.value)
-            for index_group, group in enumerate(self.origin.tokens)
-            for index_elmnt, token in enumerate(group.flatten())
+        return map(self.__token_2_node, self.source.tokens)
+
+    def __token_2_node(self, token: sqlparse.sql.Statement) -> SqlAstNode:
+        children_source = token.tokens if token.is_group else []
+        return SqlAstNode(
+            children=map(self.__token_2_node, children_source),
+            nr_children=len(children_source),
+            value=token.value,
+            family=str(token.ttype),
         )
 
 
-def _sqlparse_ast_2_sql(s: sqlparse.sql.Statement) -> SqlParseRawSqlAst:
-    s_option = _sqlparse_statement_2_known_option(s)
-    return SqlParseRawSqlAst(s_option, s)
+def _sqlparse_ast_2_sql(s: sqlparse.sql.Statement) -> SqlParseSqlAst:
+    return SqlParseSqlAst(s, _sqlparse_statement_2_known_command(s))
 
 
-def parse(raw_sql: str) -> Iterator[RawSqlAst]:
+def parse(raw_sql: str) -> Iterator[SqlParseSqlAst]:
     """
-    Returns an Abstract Syntax Tree-ish represtentation of the raw SQL queries.
+    Returns an Abstract Syntax Tree represtentation of the raw SQL queries.
     Each SQL statement in the SQL query becomes a separate item yielded by the iterator.
 
     Args:
         raw_sql (str): an SQL query.
 
     Returns:
-        Iterator[RawSqlAst]: an iterator with custom RawSqlAst objects
+        Iterator[SqlAst]: an iterator with custom SqlAst objects
 
     Examples:
         >>> l1 = list(parse("select * from foo;"))
         >>> (len(l1), l1)
-        (1, [SqlParseRawSqlAst(option=<KnownSqlOptions.SELECT: 1>, origin=<...>)])
+        (1, [SqlParseSqlAst(source=<...>, command=<KnownSqlCommand.SELECT: 1>)])
 
         >>> l2 = list(parse("select * from foo; select * from bar;"))
         >>> (len(l2), l2)
-        (2, [SqlParseRawSqlAst(option=<KnownSqlOptions.SELECT: 1>, origin=<...>), SqlParseRawSqlAst(option=<KnownSqlOptions.SELECT: 1>, origin=<...>)])
+        (2, [SqlParseSqlAst(source=<...>, command=<KnownSqlCommand.SELECT: 1>), SqlParseSqlAst(source=<...>, command=<KnownSqlCommand.SELECT: 1>)])
     """
     asts = _get_sqlparse_asts(raw_sql)
     return (_sqlparse_ast_2_sql(s) for s in asts)
