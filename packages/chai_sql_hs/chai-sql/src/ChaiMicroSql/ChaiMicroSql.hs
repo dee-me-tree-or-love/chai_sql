d541ecbbc469fffb6aaeb45ebbceb44c0b831782
Dmitrii Orlov <dmtree.dev@yahoo.com>
2023-04-26 22:26:49 +0200

    chore: more small changes

--- packages/chai_sql_hs/chai-sql/src/ChaiMicroSql/ChaiMicroSql.hs
+++ packages/chai_sql_hs/chai-sql/src/ChaiMicroSql/ChaiMicroSql.hs
@@ -1,61 +1,77 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module ChaiMicroSql.ChaiMicroSql ( ) where
 
-import           Data.List  (find)
-import qualified Data.Map   as M
-import           Data.Maybe (fromMaybe)
+import qualified Data.List as L (find)
+import qualified Data.Map  as M
+
 
 -- Basic AST
+-- ~~~~~~~~~
 
 newtype AstSqlQuery
     = AstSqlQuery AstSelectStatement
+    deriving (Show, Eq)
 
 data AstSelectStatement
     = AstBasicSelectStatement AstSelectHead AstSelectFrom
+    deriving (Show, Eq)
 
 data AstSelectHead
     = AstSelectHeadAll
     | AstSelectHeadColumns [AstSelectHeadColumn]
+    deriving (Show, Eq)
 
 data AstSelectHeadColumn
     = AstSelectHeadColumn {selectColumnName:: String, selectColumnSource:: String, selectColumnAlias:: Maybe String}
     -- TODO: support non-fully qualified column access
     -- = AstSelectHeadColumn {selectColumnAttribute:: String, selectColumnSource:: Maybe String, selectColumnAlias:: Maybe String}
+    deriving (Show, Eq)
 
 newtype AstSelectFrom
     = AstSelectFromSources [AstSelectFromSource]
+    deriving (Show, Eq)
 
 data AstSelectFromSource
     = AstSelectFromSourceTable {selectFromSourceTableName:: String, selectFromSourceTableAlias:: Maybe String}
     -- TODO: add supported to nested queries
     -- | AstSelectFromSourceQuery {selectFromSourceQuery:: AstSqlQuery, selectFromSourceQueryAlias:: Maybe String}
+    deriving (Show, Eq)
 
 
 -- Basic Type System
+-- ~~~~~~~~~~~~~~~~~~
 
--- Type wrapper
+-- Type Variables
 
-
-data BasicTypeVar
-    = BasicTypeVarTotal
-    | BasicTypeVarUnit String
-    | BasicTypeVarRecord TypeContainer
+data TypeVariable
+    = TypeVariableTotal
+    | TypeVariableUnit String
+    | TypeVariableRecord TypeContainer
+    deriving (Show, Eq)
 
 type TypeContainer = [TypeAddress]
 
-data TypeAddress = TypeAddress {typeAddressKey:: String, typeAddressValue :: BasicTypeVar}
+data TypeAddress = TypeAddress {typeAddressKey:: String, typeAddressValue :: TypeVariable}
+    deriving (Show, Eq)
 
-tuplify :: TypeAddress -> (String, BasicTypeVar)
+-- Type manipulations
+
+tuplify :: TypeAddress -> (String, TypeVariable)
 tuplify (TypeAddress k v) = (k, v)
 
-findAttribute :: String -> TypeContainer -> Maybe BasicTypeVar
-findAttribute k xs = fmap typeAddressValue (find (\c -> k == typeAddressKey c) xs)
+find :: String -> TypeContainer -> Maybe TypeVariable
+find k xs = fmap typeAddressValue (L.find (\c -> k == typeAddressKey c) xs)
 
 
 -- Context
+-- ~~~~~~~
 
-type TypeRecord = (M.Map String BasicTypeVar)
+type TypeRecord = (M.Map String TypeVariable)
 
 newtype BasicContext a = BasicContext a
+
+-- Context utils
 
 instance Functor BasicContext where
     fmap :: (a -> b) -> BasicContext a -> BasicContext b
@@ -73,25 +89,35 @@ instance Monad BasicContext where
   (>>=) :: BasicContext a -> (a -> BasicContext b) -> BasicContext b
   BasicContext a >>= f = f a
 
-type BasicTypeContext = BasicContext TypeRecord
+type TypeBasicContext = BasicContext TypeRecord
 
-emptyContext :: BasicTypeContext
+-- Context manipulation
+
+emptyContext :: TypeBasicContext
 emptyContext = pure M.empty
 
--- Type wrapping
+unionContexts :: TypeBasicContext -> TypeBasicContext -> TypeBasicContext
+unionContexts g1 g2 = do
+    m1 <- g1
+    M.union m1 <$> g2
 
-data TypeWrapper c t = TypeWrapper {context:: c, typevalue:: t}
+
+-- Type Operations
+-- ~~~~~~~~~~~~~~~
 
 -- Response wrapping
 
-type TypeJudgement = Maybe BasicTypeVar
+newtype TypeErrorMessage = TypeErrorMessage String
 
--- Type Operations
+type TypeJudgement = Either TypeErrorMessage TypeVariable
 
-merge :: BasicTypeVar -> BasicTypeVar -> TypeJudgement
-merge BasicTypeVarTotal x                           = pure x
-merge (BasicTypeVarRecord p) (BasicTypeVarRecord q) = pure $ BasicTypeVarRecord $ p ++ q
-merge _ _                                           = Nothing
+-- Type judgements
+
+merge :: TypeVariable -> TypeVariable -> TypeJudgement
+merge TypeVariableTotal x                           = pure x
+merge (TypeVariableRecord p) (TypeVariableRecord q) = pure $ TypeVariableRecord $ p ++ q
+merge v u                                           = Left $ TypeErrorMessage m
+    where m = "Could not merge two variables: " ++ show u ++ ", and " ++ show v
 
 merge' :: TypeJudgement -> TypeJudgement -> TypeJudgement
 merge' j1 j2 = do
@@ -99,62 +125,13 @@ merge' j1 j2 = do
     t2 <- j2
     merge t1 t2
 
-unionContexts :: BasicTypeContext -> BasicTypeContext -> BasicTypeContext
-unionContexts g1 g2 = do
-    m1 <- g1
-    M.union m1 <$> g2
+-- Type wrapping
 
+data TypeWrapper c t = TypeWrapper {context:: c, typevalue:: t}
 
 -- AST Type inference
+-- ~~~~~~~~~~~~~~~~~~
 
 class Typed a where
     typeOf :: a -> (a, TypeJudgement)
 
-instance Typed BasicTypeContext String where
-    typeOf :: BasicTypeContext -> String -> (BasicTypeContext, TypeJudgement)
-    typeOf g@(BasicContext m) k = (g, M.lookup k m)
-
-instance Typed BasicTypeContext AstSelectFromSource where
-    typeOf :: BasicTypeContext -> AstSelectFromSource -> (BasicTypeContext, TypeJudgement)
-    typeOf g (AstSelectFromSourceTable s Nothing) = typeOf g s
-    typeOf g (AstSelectFromSourceTable s (Just a)) =
-        case mt of
-            Just t  -> (fmap (M.insert a t) c , mt)
-            Nothing -> (c, mt)
-        where (c, mt) = typeOf g s
-
-instance Typed BasicTypeContext AstSelectFrom where
-    typeOf :: BasicTypeContext -> AstSelectFrom -> (BasicTypeContext, TypeJudgement)
-    typeOf g (AstSelectFromSources xs) =
-        foldl fOp (emptyContext, Just BasicTypeVarTotal) js
-        where js = map (typeOf g) xs
-              fOp (bg, bt) (ng, nt) = (unionContexts bg ng, merge' bt nt)
-
-instance Typed BasicTypeContext AstSelectHeadColumn where
-    typeOf :: BasicTypeContext -> AstSelectHeadColumn -> (BasicTypeContext, TypeJudgement)
-    typeOf g (AstSelectHeadColumn n s a) =
-        let al = fromMaybe n a in
-        let (_, t) = typeOf g s in
-            case t of
-                Just (BasicTypeVarRecord r) -> case mft of
-                    Nothing -> (g, Nothing)
-                    Just ft  -> (g, Just (BasicTypeVarRecord [TypeAddress al ft]))
-                    where mft = findAttribute n r
-                Just _                      -> (g, Nothing)
-                Nothing                     -> (g, Nothing)
-
-instance Typed BasicTypeContext AstSelectHead where
-    typeOf :: BasicTypeContext -> AstSelectHead -> (BasicTypeContext, TypeJudgement)
-    typeOf g AstSelectHeadAll          = (g, Just BasicTypeVarTotal)
-    typeOf g (AstSelectHeadColumns xs) =
-        foldl fOp (emptyContext, Just BasicTypeVarTotal) js
-        where js = map (typeOf g) xs
-              fOp (bg, bt) (ng, nt) = (unionContexts bg ng, merge' bt nt)
-
-instance Typed BasicTypeContext AstSelectStatement where
-    typeOf :: BasicTypeContext -> AstSelectStatement -> (BasicTypeContext, TypeJudgement)
-    typeOf g (AstBasicSelectStatement s f) =
-        let (g', tf) = typeOf g f in
-        let (_, ts) = typeOf (unionContexts g g') s in
-            undefined
-            -- FIXME: solve this using the projection
