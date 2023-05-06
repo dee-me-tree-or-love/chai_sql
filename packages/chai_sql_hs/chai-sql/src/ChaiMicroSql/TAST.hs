a93443963b2d1551f50e81e76df4f30090f071b3
Dmitrii Orlov <dmtree.dev@yahoo.com>
2023-05-06 12:49:08 +0200

    chore: WIP type system refinement

--- packages/chai_sql_hs/chai-sql/src/ChaiMicroSql/TAST.hs
+++ packages/chai_sql_hs/chai-sql/src/ChaiMicroSql/TAST.hs
@@ -5,16 +5,16 @@ import qualified Data.Map as M
 -- ~~~~~~~~~~~~~~~~~~~~~~~
 
 -- | A basic simple type representation.
+--
 -- Supported features are:
--- - 3 base types: @Bool, Number, Text@
--- - A recursive record type: e.g. @[key<String>: value<TASTSimpleType>]@
+--
+-- - 3 atomic types: @TASTAtomicType@ -> @Bool, Number, Text@
+-- - A record type without duplicate keys: e.g. @{key<String>: value<TASTAtomicType>}@
 -- - A special total record: e.g. @TOT@
 -- - A list of all above mentioned types
---
 data TASTSimpleType
     = TASTSimpleTypeBasic TASTSimpleTypeBasic   -- ^ 3 base types: @Bool, Number, Text@
     | TASTSimpleTypeRecord TASTSimpleTypeRecord -- ^ A recursive record type: e.g. @[key<String>: value<TASTSimpleType>]@
-    | TASTSimpleTypeRecordTotal                 -- ^ A special total record.
     | TASTSimpleTypeList  TASTSimpleTypeList    -- ^ A list of all possible simple types.
     deriving (Show, Eq)
 
@@ -23,21 +23,37 @@ type TASTSimpleTypeList = [TASTSimpleType]
 
 -- | All supported basic types.
 data TASTSimpleTypeBasic
-    = TASTSimpleTypeBasicBool                               -- ^ @Bool@
-    | TASTSimpleTypeBasicNumber                             -- ^ @Number@
-    | TASTSimpleTypeBasicText                               -- ^ @Text@
+    = TASTSimpleTypeBasicAtomic TASTAtomicType              -- ^ An atomic type @Bool | Number | Text@
     | TASTSimpleTypeBasicIndex TASTSimpleTypeBasicIndex     -- ^ An index tuple @key<String>: value<TASTSimpleType>@
     deriving (Show, Eq)
 
--- | A type used to index type records.
-data TASTSimpleTypeBasicIndex = TASTSimpleTypeBasicIndexKeyValue TASTSimpleTypeBasicIndexKey TASTSimpleType
+-- | Basic atomic types: @Bool | Number | Text@
+data TASTAtomicType
+    = TASTAtomicTypeBool    -- ^ @Bool@
+    | TASTAtomicTypeNumber  -- ^ @Number@
+    | TASTAtomicTypeText    -- ^ @Text@
+    deriving (Show, Eq)
+
+-- | A type used to index type collections.
+data TASTSimpleTypeBasicIndex
+    = TASTSimpleTypeRecordTotal                                   -- ^ A special total record.
+    | TASTSimpleTypeBasicIndexPair TASTSimpleTypeBasicIndexPair   -- ^ A key-value pari for an atomic type.
+    deriving (Show, Eq)
+
+-- | A type used to construct key-value index pairs for type collections.
+data TASTSimpleTypeBasicIndexPair = TASTSimpleTypeBasicIndexKeyValue TASTSimpleTypeBasicIndexKey TASTAtomicType   -- ^ A key-value pari for an atomic type.
     deriving (Show, Eq)
 
 -- | A type key-value type record map.
-type TASTSimpleTypeRecord = (M.Map TASTSimpleTypeBasicIndexKey TASTSimpleType)
+type TASTSimpleTypeRecord = (M.Map TASTSimpleTypeBasicIndexKey TASTAtomicType)
 
 emptyTypeRecord :: TASTSimpleTypeRecord
 emptyTypeRecord = M.empty
+
+-- | A specialized type construct representing DB query results.
+--
+-- - A view type with possible duplicate keys: e.g. @[key<String>: value<TASTAtomicType>]@
+type TASTDbView = [TASTSimpleTypeBasicIndex]
 
 -- Common utilities
 -- ^^^^^^^^^^^^^^^^
@@ -46,13 +62,13 @@ emptyTypeRecord = M.empty
 newtype TASTSimpleTypeBasicIndexKey = TASTSimpleTypeBasicIndexKey String deriving (Show, Eq, Ord)
 
 -- | Creates a new record
-make :: [TASTSimpleTypeBasicIndex] -> TASTSimpleTypeRecord
+make :: [TASTSimpleTypeBasicIndexPair] -> TASTSimpleTypeRecord
 make is = M.fromList $ map tuplify is
 
 -- | Tuplify the TASTSimpleTypeBasicIndex.
-tuplify :: TASTSimpleTypeBasicIndex -> (TASTSimpleTypeBasicIndexKey, TASTSimpleType)
+tuplify :: TASTSimpleTypeBasicIndexPair -> (TASTSimpleTypeBasicIndexKey, TASTAtomicType)
 tuplify (TASTSimpleTypeBasicIndexKeyValue k v) = (k,v)
 
 -- | Type record retrieval
-get :: TASTSimpleTypeBasicIndexKey -> TASTSimpleTypeRecord -> Maybe TASTSimpleType
+get :: TASTSimpleTypeBasicIndexKey -> TASTSimpleTypeRecord -> Maybe TASTAtomicType
 get = M.lookup
